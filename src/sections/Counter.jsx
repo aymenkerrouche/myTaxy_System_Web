d8b1c09e1133b72622c0b8577efa7b2281ff46db
Aymen <aymene.kerrouche@univ-constantine2.dz>
2025-02-09 21:55:42 +0100

    Counter

--- src/sections/Counter.jsx
+++ src/sections/Counter.jsx
@@ -4,7 +4,7 @@ import {wave } from "../assets";
 
 function Counter() {
 
-  const openingDate = "2024-11-01T00:00:00";
+  const openingDate = "2025-04-01T00:00:00";
   return (
     <div id="Counter" className="relative w-full h-full">
       <div className="absolute top-0 left-0 w-full h-auto -z-50 opacity-30">
